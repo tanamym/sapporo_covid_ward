b880d4214f17d38e6ae296ac080a5966faed2562
Mayumi Tanahashi <66995058+tanamym@users.noreply.github.com>
2021-09-14 00:18:49 +0900

    Add files via upload

---
+++ readtext0914.R
@@ -0,0 +1,65 @@
+library(rvest)
+library(dplyr)
+library(stringr)
+library(lubridate)
+repeat{
+  while(wday(Sys.Date(),label = TRUE)=="月"){
+    path<-"https://www.city.sapporo.jp/hokenjo/f1kansen/2019n-covhassei.html"
+    RH<-read_html(path)
+    tb<-RH%>%
+      rvest::html_nodes("tbody")
+    date<-
+      RH%>%
+      html_nodes("h3")%>%
+      html_text()%>%
+      data.frame()%>%
+      filter(str_detect(.,"区ごとの発生状況"))%>%
+      rename("Date"=".")%>%
+      mutate(Date=str_remove(Date,"区ごとの発生状況（"),
+             Date=str_remove(Date,"分）"))
+    text<-
+      tb[[7]]%>%
+      rvest::html_nodes("tr")%>%
+      rvest::html_text()
+    K<-
+      text[1]%>%
+      strsplit("\r\n")%>%
+      data.frame()
+    colnames(K)<-"区名"
+    K<-K%>%
+      filter(str_detect(区名,"区"))%>%
+      mutate(区名=str_remove_all(区名," "))
+    N<-
+      text[2]%>%
+      strsplit("\r\n")%>%
+      data.frame()
+    colnames(N)<-"count"  
+    N<-N%>%
+      filter(str_detect(count,"[0-9]"))%>%
+      filter(!str_detect(count,"\\(.+?\\)"))%>%
+      mutate(count=str_remove_all(count," "))
+    data1<-cbind(K,N)%>%
+      mutate(count=as.numeric(count))%>%
+      mutate(D=date$Date[1])%>%
+      mutate(Date1=str_sub(D,1,5),
+             Date2=str_sub(D,-5,-1),
+             Date1=str_remove(Date1,"～"),
+             Date2=str_remove(Date2,"～"))%>%
+      mutate(Date1=paste0("2021年",Date1),
+             Date2=paste0("2021年",Date2))%>%
+      mutate(Date1=as.Date(Date1,"%Y年%m月%d日"),
+             Date2=as.Date(Date2,"%Y年%m月%d日"))
+    data2<-data.frame()
+    data2<-read.csv("札幌市区別データ.csv",encoding="UTF-8")%>%
+      arrange(desc(Date1))
+    if(data1[1,4]!=data2[1,4]){
+      data3<-rbind(data2,data1)
+      write.csv(data3,"札幌市区別データ2.csv",
+                row.names=F,fileEncoding="UTF-8")
+      write.csv(data1,paste0("札幌市区別データ",Sys.Date(),".csv"),
+                row.names=F)
+    }
+    
+    sleep(3600)
+  }
+}
